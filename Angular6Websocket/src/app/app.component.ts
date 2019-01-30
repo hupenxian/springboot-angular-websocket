bfab445ff819f5a913208b1259d32eab244b5e36
hpx <m15257990085@163.com>
2019-01-30 11:35:34 +0800

    init

---
+++ Angular6Websocket/src/app/app.component.ts
@@ -0,0 +1,64 @@
+import { Component } from '@angular/core';
+import * as Stomp from '@stomp/stompjs';
+import * as SockJS from 'sockjs-client';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  description = 'Angular-WebSocket Demo';
+
+  greetings: string[] = [];
+  disabled = true;
+  name: string;
+  private stompClient = null;
+
+  constructor() { }
+
+  setConnected(connected: boolean) {
+    this.disabled = !connected;
+
+    if (connected) {
+      this.greetings = [];
+    }
+  }
+
+  connect() {
+    const socket = new SockJS('http://localhost:8080/connect');
+    this.stompClient = Stomp.over(socket);
+
+    const _this = this;
+    this.stompClient.connect({}, function (frame) {
+      _this.setConnected(true);
+      console.log('Connected: ' + frame);
+
+      _this.stompClient.subscribe('/topic/hi', function (hello) {
+        console.log(1);
+        _this.showGreeting(JSON.parse(hello.body).greeting);
+      });
+    });
+  }
+
+  disconnect() {
+    if (this.stompClient != null) {
+      this.stompClient.disconnect();
+    }
+
+    this.setConnected(false);
+    console.log('Disconnected!');
+  }
+
+  sendName() {
+    this.stompClient.send(
+      '/hello',
+      {},
+      JSON.stringify({ 'name': this.name })
+    );
+  }
+
+  showGreeting(message) {
+    this.greetings.push(message);
+  }
+}
